b4da37b38b13e5643620bff79d17e285a0034529
Hunab <bohorquez.atha@gmail.com>
2026-02-16 23:07:07 -0600

    terminamos el ejercicio del dispositivo

---
+++ TP/Dispositivo.cs
@@ -0,0 +1,36 @@
+﻿public class Dispositivo
+{
+    // Atributos - Campos 
+    private string nombre;
+    private bool encendido;
+    private int consumo;
+
+    // Propiedades
+    public string Nombre
+    { 
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public bool Encendido
+    {
+        get { return encendido; }
+    }
+
+    public int Consumo
+    {
+        get { return consumo; }
+        set { consumo = value; }
+    }
+
+
+    // Constructor
+    public Dispositivo(string nombre, int consumo)
+    {
+        this.nombre = nombre;
+        this.consumo = consumo;
+        this.encendido = false; //inicia en false
+    }
+
+    // Metodos
+}
