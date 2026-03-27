482267358aed4d132ac7870a0ee7740aead7c001
Bohorquez <hunab.bohorquez@ingenieria.unam.edu>
2026-03-27 10:48:06 -0600

    Clase 24

--- TP/24_3_Ejercicio.cs
+++ TP/24_3_Ejercicio.cs
@@ -1,1 +1,57 @@
-﻿
+﻿// Clase que representa Estudiantes
+
+using System.Runtime.CompilerServices;
+
+public class Estudiante
+{
+    // Atributos
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public double Calificacion { get; set; }
+
+    // Constructor
+
+    // Metodos
+    public override string ToString()
+    {
+        return $"ID: {Id}, Nombre: {Nombre}, Edad: {Edad}, Calificacion: {Calificacion}";
+    }
+}
+
+// Clase para manejar el arhivo de estudiantes
+
+public class GestorEstudiantes
+{
+    // ATributos
+    private string rutaArchivo;
+
+    // COnstructor
+    public GestorEstudiantes(string ruta)
+    {
+        rutaArchivo = ruta;
+    }
+
+    // Metodo para guardar la lista de estudiantes en un archivo
+
+    public void GuardarEstudiantes(List<Estudiantes> estudiantes)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(rutaArchivo))
+            {
+                foreach (Estudiante estudiante in estudiantes)
+                {
+                    writer.WriteLine(estudiante.ToString());
+                }    
+            }
+
+
+        }
+        catch
+        {
+
+        }
+    }
+
+}
